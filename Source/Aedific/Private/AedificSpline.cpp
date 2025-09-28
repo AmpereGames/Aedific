4f870cbbd1f4a5ab508a8e383344d6b05b82b918
Obiwan Medeiros <obiwanws@gmail.com>
2025-09-28 14:48:46 -0300

    Added spline logic for mesh creation

--- Source/Aedific/Private/AedificSpline.cpp
+++ Source/Aedific/Private/AedificSpline.cpp
@@ -1,6 +1,9 @@
-// Copyright (c) 2025 Pixane.
+﻿// Copyright (c) 2025 Pixane.
 
 #include "AedificSpline.h"
+
+#include <Components/SplineComponent.h>
+#include <Components/SplineMeshComponent.h>
 
 #if WITH_EDITORONLY_DATA
 #include <Components/BillboardComponent.h>
@@ -15,8 +18,49 @@ AAedificSpline::AAedificSpline()
 	PrimaryActorTick.bStartWithTickEnabled = false;
 	PrimaryActorTick.bCanEverTick = false;
 
+	// Set default values for AAedificSpline members.
+	SplineMeshComponents.Empty();
+	StaticMesh = nullptr;
+	DefaultStaticMesh = nullptr;
+	Material = nullptr;
+	DefaultMaterial = nullptr;
+	bInterpRoll = false;
+	bAbsoluteUpDirection = false;
+	UpDirection = FVector::UpVector;
+	TangentScalingFactor = 1.f;
+
+	// Create scene component.
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	RootComponent = SceneComponent;
+	SceneComponent->SetMobility(EComponentMobility::Static);
+	SceneComponent->SetComponentTickEnabled(false);
+
+	// Create spline component.
+	SplineComponent = CreateDefaultSubobject<USplineComponent>(TEXT("SplineComponent"));
+	SplineComponent->SetupAttachment(RootComponent);
+	SplineComponent->SetMobility(EComponentMobility::Static);
+	SplineComponent->SetComponentTickEnabled(false);
+
+	// Fetch default mesh file.
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> floorMeshFile(TEXT("/Aedific/Meshes/SM_Floor_Decimated.SM_Floor_Decimated"));
+	if (floorMeshFile.Succeeded())
+	{
+		// Assign the mesh to the static mesh reference.
+		DefaultStaticMesh = floorMeshFile.Object;
+		StaticMesh = DefaultStaticMesh;
+	}
+
+	// Fetch default material file.
+	static ConstructorHelpers::FObjectFinder<UMaterialInterface> floorMaterialFile(TEXT("/Aedific/Materials/MI_Blockout_Basic.MI_Blockout_Basic"));
+	if (floorMaterialFile.Object)
+	{
+		// Assign the material to the material reference.
+		DefaultMaterial = floorMaterialFile.Object;
+		Material = DefaultMaterial;
+	}
+
 #if WITH_EDITORONLY_DATA
-	// Create Editor sprite.
+	// Create editor sprite.
 	EditorSprite = CreateEditorOnlyDefaultSubobject<UBillboardComponent>(TEXT("SplineSpriteComponent"));
 	if (!IsRunningCommandlet() && EditorSprite)
 	{
@@ -38,4 +82,208 @@ AAedificSpline::AAedificSpline()
 		EditorSprite->bIsScreenSizeScaled = true;
 	}
 #endif // WITH_EDITORONLY_DATA
+}
+
+#if WITH_EDITOR
+void AAedificSpline::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	if (PropertyChangedEvent.GetPropertyName() == FName(TEXT("StaticMesh")))
+	{
+		if (!StaticMesh)
+		{
+			// Assign the mesh to the static mesh reference.
+			StaticMesh = DefaultStaticMesh;
+		}
+		GenerateMeshes();
+	}
+	else if(PropertyChangedEvent.GetPropertyName() == FName(TEXT("Material")))
+	{
+		if (!Material)
+		{
+			Material = DefaultMaterial;
+		}
+		UpdateMaterials();
+	}
+}
+#endif // WITH_EDITOR
+
+void AAedificSpline::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	Rebuild();
+}
+
+void AAedificSpline::Rebuild()
+{
+	GenerateMeshes();
+}
+
+void AAedificSpline::CalculateTangents()
+{
+	const int32 PointsNum = SplineComponent->GetNumberOfSplinePoints();
+	if (PointsNum > 1)
+	{
+		for (int32 i = 0; i < PointsNum; i++)
+		{
+			const FVector P = SplineComponent->GetLocationAtSplinePoint(i, ESplineCoordinateSpace::Local);
+
+			FVector Incoming = FVector::ZeroVector;
+			FVector Outgoing = FVector::ZeroVector;
+
+			if (i == 0) // first point
+			{
+				const FVector P1 = SplineComponent->GetLocationAtSplinePoint(1, ESplineCoordinateSpace::Local);
+				const FVector Dir = (P1 - P).GetSafeNormal();
+
+				// Incoming stays zero
+				Outgoing = Dir * (P1 - P).Length() / TangentScalingFactor;
+			}
+			else if (i == PointsNum - 1) // last point
+			{
+				const FVector Pn1 = SplineComponent->GetLocationAtSplinePoint(PointsNum - 2, ESplineCoordinateSpace::Local);
+				const FVector Dir = (P - Pn1).GetSafeNormal();
+
+				// Outgoing stays zero
+				Incoming = Dir * (P - Pn1).Length() / TangentScalingFactor;
+			}
+			else // interior point
+			{
+				const FVector Prev = SplineComponent->GetLocationAtSplinePoint(i - 1, ESplineCoordinateSpace::Local);
+				const FVector Next = SplineComponent->GetLocationAtSplinePoint(i + 1, ESplineCoordinateSpace::Local);
+
+				const FVector Dir = (Next - Prev).GetSafeNormal();
+
+				Incoming = Dir * (P - Prev).Length() / TangentScalingFactor;
+				Outgoing = Dir * (P - Next).Length() / TangentScalingFactor;
+			}
+
+			SplineComponent->SetTangentsAtSplinePoint(i, Incoming, Outgoing, ESplineCoordinateSpace::Local, false);
+		}
+
+		SplineComponent->UpdateSpline();
+		GenerateMeshes();
+	}
+}
+
+void AAedificSpline::Reset()
+{
+	SplineComponent->ResetToDefault();
+
+	GenerateMeshes();
+
+	MarkComponentsRenderStateDirty();
+}
+
+void AAedificSpline::EmptyMeshes()
+{
+	if (SplineMeshComponents.Num() > 0)
+	{
+		for (USplineMeshComponent* spline : SplineMeshComponents)
+		{
+			spline->DestroyComponent();
+		}
+
+		SplineMeshComponents.Reset();
+	}
+}
+
+void AAedificSpline::GenerateMeshes()
+{
+	if (StaticMesh)
+	{
+		EmptyMeshes();
+
+		StaticMesh->CalculateExtendedBounds();
+		
+		const float MeshLength = StaticMesh->GetBoundingBox().GetSize().X;
+		const float SplineLeght = SplineComponent->GetSplineLength();
+		const int32 LoopSize = FMath::CeilToInt(SplineLeght / MeshLength);
+
+		FVector StartLocation = FVector::ZeroVector;
+		FVector EndLocation = FVector::ZeroVector;
+		FVector StartTangent = FVector::ZeroVector;
+		FVector EndTangent = FVector::ZeroVector;
+		FVector UpVector = FVector::UpVector;
+		float StartDistance = 0.f;
+		float MidDistance = 0.f;
+		float EndDistance = 0.f;
+
+		SplineMeshComponents.Reset(LoopSize);
+
+		for (int32 i = 0; i < LoopSize; i++)
+		{
+			if (SplineLeght < MeshLength)
+			{
+				StartDistance = 0.f;
+				EndDistance = SplineLeght;
+			}
+			else
+			{
+				StartDistance = i * MeshLength;
+				EndDistance = StartDistance + MeshLength;
+			}
+
+			if (EndDistance >= SplineLeght)
+			{
+				EndDistance = SplineLeght;
+			}
+
+			MidDistance = StartDistance + (EndDistance - StartDistance) / 2.f;
+
+			StartLocation = SplineComponent->GetLocationAtDistanceAlongSpline(StartDistance, ESplineCoordinateSpace::Local);
+			EndLocation = SplineComponent->GetLocationAtDistanceAlongSpline(EndDistance, ESplineCoordinateSpace::Local);
+
+			StartTangent = SplineComponent->GetTangentAtDistanceAlongSpline(StartDistance, ESplineCoordinateSpace::Local).GetClampedToMaxSize(MeshLength);
+			EndTangent = SplineComponent->GetTangentAtDistanceAlongSpline(EndDistance, ESplineCoordinateSpace::Local).GetClampedToMaxSize(MeshLength);
+
+			if (bAbsoluteUpDirection)
+			{
+				UpVector = UpDirection;
+			}
+			else
+			{
+				UpVector = SplineComponent->GetUpVectorAtDistanceAlongSpline(MidDistance, ESplineCoordinateSpace::Local);
+			}
+
+			const FString SegmentName = "SplineMesh" + FString::SanitizeFloat(i);
+			USplineMeshComponent* NewSplineMeshComponent = NewObject<USplineMeshComponent>(this, USplineMeshComponent::StaticClass(), *SegmentName, EObjectFlags::RF_Transactional);
+			NewSplineMeshComponent->CreationMethod = EComponentCreationMethod::UserConstructionScript;
+			NewSplineMeshComponent->RegisterComponent();
+			NewSplineMeshComponent->AttachToComponent(SplineComponent, FAttachmentTransformRules::FAttachmentTransformRules(EAttachmentRule::KeepRelative, true));
+			NewSplineMeshComponent->SetMobility(EComponentMobility::Static);
+			
+			NewSplineMeshComponent->SetComponentTickEnabled(false);
+			NewSplineMeshComponent->bSmoothInterpRollScale = bInterpRoll;
+
+			NewSplineMeshComponent->SetForwardAxis(ESplineMeshAxis::X);
+			NewSplineMeshComponent->SetSplineUpDir(UpVector);
+			NewSplineMeshComponent->SetStartAndEnd(StartLocation, StartTangent, EndLocation, EndTangent, true);
+
+			NewSplineMeshComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndProbe);
+
+			if (StaticMesh)
+			{
+				NewSplineMeshComponent->SetStaticMesh(StaticMesh);
+			}
+
+			if (Material)
+			{
+				NewSplineMeshComponent->SetMaterial(0, Material);
+			}
+		}
+	}
+}
+
+void AAedificSpline::UpdateMaterials()
+{
+	if (Material)
+	{
+		for (USplineMeshComponent* spline : SplineMeshComponents)
+		{
+			spline->SetMaterial(0, Material);
+		}
+	}
 }
--- Source/Aedific/Public/AedificSpline.h
+++ Source/Aedific/Public/AedificSpline.h
@@ -6,6 +6,9 @@
 
 #include "AedificSpline.generated.h"
 
+class USplineComponent;
+class USplineMeshComponent;
+
 /**
  * Actor class to created spline based meshes.
  */
@@ -18,9 +21,83 @@ public:
 	/** Sets default values for this actor's properties. */
 	AAedificSpline();
 
+protected:
+	/** Scene component. */
+	UPROPERTY()
+	TObjectPtr<USceneComponent> SceneComponent;
+
+	/** Spline component. */
+	UPROPERTY(EditInstanceOnly, Category = Aedific)
+	TObjectPtr<USplineComponent> SplineComponent;
+
+	/** Mesh. */
+	UPROPERTY(EditInstanceOnly, Category = Aedific)
+	TObjectPtr<UStaticMesh> StaticMesh;
+
+	/** Material. */
+	UPROPERTY(EditInstanceOnly, Category = Aedific)
+	TObjectPtr<UMaterialInterface> Material;
+
+	/**  */
+	UPROPERTY(EditInstanceOnly, Category = Aedific)
+	uint8 bInterpRoll : 1;
+
+	/**  */
+	UPROPERTY(EditInstanceOnly, Category = Aedific, Meta = (InlineEditConditionToggle))
+	uint8 bAbsoluteUpDirection : 1;
+
+	/**  */
+	UPROPERTY(EditInstanceOnly, Category = Aedific, Meta = (EditCondition = "bAbsoluteUpDirection"))
+	FVector UpDirection;
+
+	/**  */
+	UPROPERTY(EditInstanceOnly, Category = Aedific)
+	float TangentScalingFactor;
+
+	//~ Begin of UObject implementation.
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif // WITH_EDITOR
+	//~ End of UObject implementation.
+
+	//~ Begin of AActor implementation.
+	virtual void OnConstruction(const FTransform& Transform) override;
+	//~ End of AActor implementation.
+
+	/**  */
+	void GenerateMeshes();
+
+	/**  */
+	void UpdateMaterials();
+
+	/**  */
+	void EmptyMeshes();
+
+	/**  */
+	UFUNCTION(CallInEditor, Category = Aedific)
+	void Rebuild();
+
+	/**  */
+	UFUNCTION(CallInEditor, Category = Aedific)
+	void Reset();
+
+	/**  */
+	UFUNCTION(CallInEditor, Category = Aedific)
+	void CalculateTangents();
+
 private:
 #if WITH_EDITORONLY_DATA
 	/** Sprite to show the Actor's sprite in Editor. */
 	TObjectPtr<UBillboardComponent> EditorSprite;
 #endif // WITH_EDITORONLY_DATA
+
+	/**  */
+	UPROPERTY()
+	TArray<TObjectPtr<USplineMeshComponent>> SplineMeshComponents;
+
+	/** Mesh. */
+	TObjectPtr<UStaticMesh> DefaultStaticMesh;
+
+	/** Material. */
+	TObjectPtr<UMaterialInterface> DefaultMaterial;
 };
